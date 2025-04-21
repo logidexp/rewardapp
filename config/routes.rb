9132faa477d62b85661517bbdf1ec2e2de6fa2be
FreeFly0125 <135254388+FreeFly0125@users.noreply.github.com>
2025-04-21 09:25:00 -0700

    Cleanup :package:

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,6 @@ Rails.application.routes.draw do
            controllers: {
              sessions: "users/sessions"
            }
-  resources :points_events
-  resources :bonuses
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,32 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Routes', type: :routing do
-  describe 'users routes' do
-    it 'routes to #index' do
-      expect(get('/users')).to route_to('users#index')
-    end
-
-    it 'routes to #show' do
-      expect(get('/users/1')).to route_to('users#show', id: '1')
-    end
-
-    it 'routes to #create' do
-      expect(post('/users')).to route_to('users#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put('/users/1')).to route_to('users#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch('/users/1')).to route_to('users#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete('/users/1')).to route_to('users#destroy', id: '1')
-    end
-  end
-
   describe 'api v1 rewards routes' do
     it 'routes to #index' do
       expect(get('/api/v1/rewards')).to route_to('api/v1/rewards#index')
@@ -55,58 +29,6 @@ RSpec.describe 'Routes', type: :routing do
 
     it 'does not route to #destroy' do
       expect(delete('/api/v1/rewards/1')).not_to be_routable
-    end
-  end
-
-  describe 'bonuses routes' do
-    it 'routes to #index' do
-      expect(get('/bonuses')).to route_to('bonuses#index')
-    end
-
-    it 'routes to #show' do
-      expect(get('/bonuses/1')).to route_to('bonuses#show', id: '1')
-    end
-
-    it 'routes to #create' do
-      expect(post('/bonuses')).to route_to('bonuses#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put('/bonuses/1')).to route_to('bonuses#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch('/bonuses/1')).to route_to('bonuses#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete('/bonuses/1')).to route_to('bonuses#destroy', id: '1')
-    end
-  end
-
-  describe 'points_events routes' do
-    it 'routes to #index' do
-      expect(get('/points_events')).to route_to('points_events#index')
-    end
-
-    it 'routes to #show' do
-      expect(get('/points_events/1')).to route_to('points_events#show', id: '1')
-    end
-
-    it 'routes to #create' do
-      expect(post('/points_events')).to route_to('points_events#create')
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put('/points_events/1')).to route_to('points_events#update', id: '1')
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch('/points_events/1')).to route_to('points_events#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete('/points_events/1')).to route_to('points_events#destroy', id: '1')
     end
   end
 
